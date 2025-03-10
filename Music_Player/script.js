f4591ffd404fbef2d415480156b9c4823435e8c0
ashnichinnusam <ashnichinnusam2003@gmail.com>
2025-03-10 13:36:34 +0530

    Added Music Player Project

---
+++ Music_Player/script.js
@@ -0,0 +1,14 @@
+document.getElementById("button1").addEventListener("click",function(){
+    const audio = new Audio("audio/flaw.mp3");
+    audio.play();
+});
+
+document.getElementById("button2").addEventListener("click",function(){
+    const audio = new Audio("audio/garden.mp3");
+    audio.play();
+});
+
+document.getElementById("button3").addEventListener("click",function(){
+    const audio = new Audio("audio/night.mp3");
+    audio.play();
+});
